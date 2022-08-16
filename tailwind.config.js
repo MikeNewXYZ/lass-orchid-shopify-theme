6744c3d073fbe9093c8510f2d7eec2df434c625c
NotBaldrick <Git@NotBaldrick.com>
2022-08-16 11:53:42 +0100

    added tailwind and alpine

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+module.exports = {
+  content: [
+    "./assets/*.js",
+    "./layout/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./templates/**/*.liquid",
+    "./templates/**/*.json"
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem"
+    }
+  },
+  daisyui: {
+    themes: ["lofi"]
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+}
