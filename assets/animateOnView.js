259689bcbde4e88130e1a8eefb02ddbb6c3c2ce5
NotBaldrick <Git@NotBaldrick.com>
2022-09-07 17:26:42 +0100

    modify animateOnView to be able to animate multiple elements

--- assets/animateOnView.js
+++ assets/animateOnView.js
@@ -1,26 +1,35 @@
 const animateOnView = (
-  element,
+  observeElement,
+  animateElements = [],
   initClasses = [],
   onViewClasses = [],
   options = {
     threshold: 0.5
   }
 ) => {
-  element.classList.add(...initClasses)
+  animateElements.forEach((el) => {
+    el.classList.add(...initClasses)
+  })
 
   const observer = new window.IntersectionObserver(([entry]) => {
     if (!entry.isIntersecting) return
 
-    element.classList.remove(...initClasses)
-    element.classList.add(...onViewClasses)
+    animateElements.forEach((el) => {
+      el.classList.remove(...initClasses)
+      el.classList.add(...onViewClasses)
+  
+      el.addEventListener("animationend", (event) => {
+        if (event.target !== this) return
+        event.stopPropagation()
+        
+        el.classList.remove(...onViewClasses)
 
-    element.addEventListener("animationend", (event) => {
-      event.stopPropagation()
-      element.classList.remove(...onViewClasses)
-    }, {once: true})
+        clearTimeout(timeout)
+      }, {once: true})
+    })
 
     observer.disconnect()
   }, {root: null, threshold: options.threshold})
 
-  observer.observe(element)
+  observer.observe(observeElement)
 }
